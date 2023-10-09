4fce296fe0ffd689780941d2b6ad847a4329aa40
ozawa <omy.sk.izw.ss@gmail.com>
2023-10-09 23:58:34 +0900

    ４回目

--- src/app/Models/Author.php
+++ src/app/Models/Author.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  protected $fillable = ['name', 'age', 'nationality'];
+  public function getDetail()
+  {
+    $txt = 'ID:'.$this->id . ' ' . $this->name . '(' . $this->age .  '才'.') '.$this->nationality;
+    return $txt;
+  }
 }
