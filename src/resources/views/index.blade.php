ef8babf2c5f857aaec29e87615b3fd663cf53175
ozawa <omy.sk.izw.ss@gmail.com>
2023-10-29 17:12:40 +0900

    ６回目

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,30 +1,32 @@
 @extends('layouts.default')
 <style>
-  th {
-    background-color: #289ADC;
-    color: white;
-    padding: 5px 40px;
-  }
-  tr:nth-child(odd) td{
-    background-color: #FFFFFF;
-  }
-  td {
-    padding: 25px 40px;
+th {
+  background-color: #289ADC;
+  color: white;
+  padding: 5px 40px;
+}
+tr:nth-child(odd) td{
+  background-color: #FFFFFF;
+}
+td {
+  padding: 25px 40px;
     background-color: #EEEEEE;
-    text-align: center;
-  }
+  text-align: center;
+}
 </style>
-@section('title', 'index.blade.php')
+@section('title', 'book.index.blade.php')
 
 @section('content')
 <table>
   <tr>
-    <th>Data</th>
+    <th>Books</th>
   </tr>
-  @foreach ($authors as $author)
-    <tr>
-      <td>{{$author->getDetail()}}</td>
-    </tr>
+  @foreach ($items as $item)
+  <tr>
+    <td>
+      {{$item->getTitle()}}
+    </td>
+  </tr>
   @endforeach
 </table>
 @endsection
